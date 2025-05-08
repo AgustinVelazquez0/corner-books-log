924a373c6584cb77a6a880652228334760eaf14f
Agustin Velazquez <agubolso2@gmail.com>
2025-05-08 07:55:05 -0300

    sada

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve("./src"),
-    },
-  },
-  server: {
-    historyApiFallback: true,
-  },
-  build: {
-    outDir: "dist",
-    copyPublicDir: true, // Asegura que los archivos de public/ se copien a dist/
-  },
 });
