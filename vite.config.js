adf7ca4de95c3e6d051a6fe4e72d8aca72ef815a
Agustin Velazquez <agubolso2@gmail.com>
2025-05-08 07:40:17 -0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve("./src"),
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
   build: {
     outDir: "dist",
+    copyPublicDir: true, // Asegura que los archivos de public/ se copien a dist/
   },
 });
